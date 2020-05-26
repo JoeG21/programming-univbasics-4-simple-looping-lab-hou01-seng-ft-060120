c87ea522fadae3b6fa88f48f8265022f643e7a14
Joe Gomez <jcggomez@live.com>
2020-05-26 15:32:44 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -27,8 +27,10 @@ end
 
 def return_string_array(array)
   counter = 0
+  new_array = []
   while counter < array.length do
-    puts array[counter].to_s
+    new_array.push(array[counter].to_s)
     counter += 1
   end
+  new_array
 end
