87d033bd34b143f44f7b70bf0ddb28b984d0852c
Joe Gomez <jcggomez@live.com>
2020-05-26 15:10:08 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,24 @@
-# Write your methods here
+def loop_message_five_times(string)
+  counter = 0
+  while counter < string.length do
+  puts string
+  counter += 1
+  end
+end
+
+
+def loop_message_n_times(string, integer)
+  while integer < string.length do 
+    puts string[integer]
+  end
+end
+
+
+def output_array 
+  
+end
+
+
+def return_string_array
+
+end
