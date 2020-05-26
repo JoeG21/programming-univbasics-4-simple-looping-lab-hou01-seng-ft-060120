abf2d2e1579efba8344ca7c4a14052368cf2fab6
Joe Gomez <jcggomez@live.com>
2020-05-26 15:14:24 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -15,7 +15,11 @@ def loop_message_n_times(string, integer)
 end
 
 
-def output_array 
+def output_array(array)
+  counter = 0
+  while counter < array.length do 
+    puts array
+    counter += 1 
   
 end
 
